6136aaf6a4bf288568c1601eeef0ddfcf7029778
Andrew Barbour <andrew.barbour1@gmail.com>
2020-01-09 04:44:13 +1100

    Implemented ThreadPool (doesn't yet consider job dependencies)

--- src/Engine/Parallelism/ThreadPool.cpp
+++ src/Engine/Parallelism/ThreadPool.cpp
@@ -1,2 +1,34 @@
 #include "ThreadPool.hpp"
 
+// Constructs a new thread pool with a specified amount of threads
+AndGen::ThreadPool::ThreadPool(unsigned int threadCount)
+{
+	m_threads.reserve(threadCount);
+
+	// Create threads and start them
+	for (size_t i = 0; i < threadCount; i++)
+	{
+		m_threads.push_back(std::make_unique<PooledThread>());
+		m_threads[i]->Start();
+	}
+}
+
+// De-constructor of the Thread Pool
+AndGen::ThreadPool::~ThreadPool()
+{
+	// Wait for threads to finish executing
+	for (size_t i = 0; i < m_threads.size(); i++)
+	{
+		m_threads[i]->Stop(true);
+	}
+}
+
+// Waits for threads to complete their current tasks and become idle
+void AndGen::ThreadPool::WaitForThreads()
+{
+	// Wait for each thread to complete their execution queue
+	for (size_t i = 0; i < m_threads.size(); i++)
+	{
+		m_threads[i]->WaitForQueue();
+	}
+}
--- src/Engine/Parallelism/ThreadPool.hpp
+++ src/Engine/Parallelism/ThreadPool.hpp
@@ -4,9 +4,11 @@
 // STL includes
 #include <algorithm>
 #include <exception>
-#include <functional>
+#include <limits>
+#include <memory>
 #include <vector>
 // AndGen includes
+#include <AndGen/Engine/Jobs/Job.hpp>
 #include <AndGen/Exceptions/NotImplementedException.hpp>
 #include "..\Parallelism\PooledThread.hpp"
 
@@ -22,14 +24,15 @@ namespace AndGen
 		/// Constructs a new thread pool with a specified amount of threads
 		/// </summary>
 		/// <param name="threadCount">Amount of threads to construct within the pool</param>
-		ThreadPool(int threadCount);
+		ThreadPool(unsigned int threadCount = GetIdealThreadCount());
 		ThreadPool(const ThreadPool&)				= delete;
 		ThreadPool& operator=(const ThreadPool&)	= delete;
+
 		/// <summary>
 		/// De-constructor of the Thread Pool. 
 		/// Will wait for all threads to complete their current tasks before de-constructing.
 		/// </summary>
-		~ThreadPool() = default;
+		~ThreadPool();
 
 		/// <summary>
 		/// Returns the ideal amount of threads in the pool for maximum performance
@@ -46,44 +49,115 @@ namespace AndGen
 			return std::max<unsigned int>(0, std::thread::hardware_concurrency() - 1);
 		}
 
+		/// <summary>
+		/// Adds a job to the thread pool to execute
+		/// </summary>
+		/// <param name="job">Job to enqueue</param>
+		/// <typeparam name="JobType">Type of job</typeparam>
 		template<class JobType>
-		void QueueJob(JobType job)
+		void QueueJob(const std::shared_ptr<JobType> job)
 		{
-			throw NotImplementedException();
+			// Find value of thread with least amount of jobs queued
+			size_t lowestQueueCount = std::numeric_limits<size_t>::max();
+			for (size_t i = 0; i < m_threads.size(); i++)
+			{
+				if (m_threads[i]->GetQueue().Count() < lowestQueueCount)
+				{
+					lowestQueueCount = m_threads[i]->GetQueue().Count();
+				}
+			}
+
+			// Queue with a thread which has the least amount of jobs queued
+			for (size_t i = 0; i < m_threads.size(); i++)
+			{
+				if (m_threads[i]->GetQueue().Count() <= lowestQueueCount)
+				{
+					m_threads[i]->QueueJob(job);
+					return;
+				}
+			}
+
+			throw std::logic_error("Unknown logic error - unable to enqueue job");
 		}
+		
+		/// <summary>
+		/// Adds multiple jobs from a collection to the thread pool to execute
+		/// </summary>
+		/// <param name="beginItr">Begin iterator for collection</param>
+		/// <param name="endItr">End iterator for collection</param>
+		/// <typeparam name="JobType">Type of jobs</typeparam>
+		template<class Iterator>
+		void QueueJobs(const Iterator beginItr, const Iterator endItr)
+		{
+			for (Iterator i = beginItr; i != endItr; i++)
+			{
+				QueueJob(*i);
+			}
+		}
+
 		/// <summary>
 		/// Waits for threads to complete their current tasks and become idle
 		/// </summary>
 		void WaitForThreads();
-		/// <summary>
-		/// Changes the amount of threads within the pool
-		/// </summary>
-		void Resize(int threadCount);
 
 		/// <summary>
 		/// The amount of threads within the pool
 		/// </summary>
-		inline int Size()
+		inline unsigned int Size()
 		{
-			throw NotImplementedException();
+			return static_cast<unsigned int>(m_threads.size());
 		}
+
+		/// <summary>
+		/// The amount of Jobs currently queued to be processed
+		/// </summary>
+		inline unsigned int PendingJobsCount() const
+		{
+			unsigned int pendingJobsCount = 0;
+			for (size_t i = 0; i < m_threads.size(); i++)
+			{
+				pendingJobsCount += static_cast<unsigned int>(m_threads[i]->GetQueue().Count());
+			}
+
+			return pendingJobsCount;
+		}
+
 		/// <summary>
 		/// Returns the amount of threads currently executing a task
 		/// </summary>
-		inline int RunningCount() const
+		inline unsigned int RunningCount() const
 		{
-			throw NotImplementedException();
+			unsigned int runningCount = 0;
+			for (size_t i = 0; i < m_threads.size(); i++)
+			{
+				if (m_threads[i]->GetStatus() == PooledThread::Status::ExecutingJobs)
+				{
+					runningCount++;
+				}
+			}
+
+			return runningCount;
 		}
+
 		/// <summary>
 		/// Amount of threads currently idle and not executing a task
 		/// </summary>
-		inline int IdleCount() const
+		inline unsigned int IdleCount() const
 		{
-			throw NotImplementedException();
+			unsigned int idleCount = 0;
+			for (size_t i = 0; i < m_threads.size(); i++)
+			{
+				if (m_threads[i]->GetStatus() == PooledThread::Status::Idle)
+				{
+					idleCount++;
+				}
+			}
+
+			return idleCount;
 		}
 
 	private:
-		std::vector<PooledThread> m_threads;
+		std::vector<std::unique_ptr<PooledThread>> m_threads;
 	};
 }
 
